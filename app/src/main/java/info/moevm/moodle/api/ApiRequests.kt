9eaca2f8c32f19fa3622ccce651dea8ba35d52f5
alexstak <alex.stak@yandex.ru>
2020-12-23 21:09:00 +0300

    data structers and functions for courses screens
    Co-Authored-By: Mikuro <17966288+Mikuro@users.noreply.github.com>

--- app/src/main/java/info/moevm/moodle/api/ApiRequests.kt
+++ app/src/main/java/info/moevm/moodle/api/ApiRequests.kt
@@ -1,8 +1,6 @@
 package info.moevm.moodle.api
 
-import info.moevm.moodle.model.Course
-import info.moevm.moodle.model.CurrentCourse
-import info.moevm.moodle.model.LoginSuccess
+import info.moevm.moodle.model.*
 import retrofit2.Call
 import retrofit2.http.GET
 import retrofit2.http.Query
@@ -11,7 +9,7 @@ interface ApiRequests {
     @GET("/login/token.php")
     fun logIn(@Query("service") serviceName: String, @Query("username") userName: String, @Query("password") passWord: String): Call<LoginSuccess>
     @GET("/webservice/rest/server.php")
-    fun getCourses(@Query("wstoken") wsToken: String, @Query("wsfunction") wsFunction: String, @Query("moodlewsrestformat") moodlewsRestFormat: String): Call<Course>
+    fun getCourses(@Query("wstoken") wsToken: String, @Query("wsfunction") wsFunction: String, @Query("moodlewsrestformat") moodlewsRestFormat: String): Call<Courses>
     @GET("/webservice/rest/server.php")
-    fun getCurrentCourses(@Query("wstoken") wsToken: String, @Query("wsfunction") wsFunction: String, @Query("moodlewsrestformat") moodlewsRestFormat: String): Call<CurrentCourse>
+    fun getCurrentCourses(@Query("wstoken") wsToken: String, @Query("wsfunction") wsFunction: String, @Query("moodlewsrestformat") moodlewsRestFormat: String): Call<CurrentCourses>
 }
--- app/src/main/java/info/moevm/moodle/api/MoodleApi.kt
+++ app/src/main/java/info/moevm/moodle/api/MoodleApi.kt
@@ -1,15 +1,16 @@
 package info.moevm.moodle.api
 
+import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
-import info.moevm.moodle.model.APIVariables
-import info.moevm.moodle.model.LoginSuccess
+import info.moevm.moodle.model.*
 import okhttp3.OkHttpClient
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import timber.log.Timber
 import java.util.concurrent.TimeUnit
 
 class MoodleApi {
@@ -35,13 +36,61 @@ class MoodleApi {
                 ) {
                     val res = response.body()
                     if (response.code() == 200 && res != null) {
-                        data.value = res
+                        data.value = res;
+                        System.out.println(data.value);
                     } else {
                         data.value = null
                     }
                 }
 
                 override fun onFailure(call: Call<LoginSuccess>, t: Throwable) {
+                    data.value = null
+                }
+            })
+        return data
+    }
+
+    fun getCurrentCourses(token: String): LiveData<CurrentCourses> {
+        val data = MutableLiveData<CurrentCourses>()
+        api.getCurrentCourses(token, APIVariables.MOD_ASSIGN_GET_ASSIGMENTS.value, APIVariables.MOODLE_WS_REST_FORMAT.value)
+            .enqueue(object : Callback<CurrentCourses> { // асинхронный вызов.
+                override fun onResponse(
+                    call: Call<CurrentCourses>,
+                    response: Response<CurrentCourses>
+                ) {
+                    val res = response.body()
+                    if (response.code() == 200 && res != null) {
+                        data.value = res;
+                        System.out.println(data.value);
+                    } else {
+                        data.value = null
+                    }
+                }
+
+                override fun onFailure(call: Call<CurrentCourses>, t: Throwable) {
+                    data.value = null
+                }
+            })
+        return data
+    }
+    fun getCourses(token: String): LiveData<Courses> {
+        val data = MutableLiveData<Courses>()
+        api.getCourses(token, APIVariables.CORE_COURSE_GET_COURSES_BY_FIELD.value, APIVariables.MOODLE_WS_REST_FORMAT.value)
+            .enqueue(object : Callback<Courses> { // асинхронный вызов.
+                override fun onResponse(
+                    call: Call<Courses>,
+                    response: Response<Courses>
+                ) {
+                    val res = response.body()
+                    if (response.code() == 200 && res != null) {
+                        data.value = res;
+                        System.out.println(data.value);
+                    } else {
+                        data.value = null
+                    }
+                }
+
+                override fun onFailure(call: Call<Courses>, t: Throwable) {
                     data.value = null
                 }
             })
