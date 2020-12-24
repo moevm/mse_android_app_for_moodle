064d30c62a4eb25f67229d3c55587b809f881bd1
Nikita Pochaev <pochaev.nik@gmail.com>
2020-12-24 03:38:07 +0300

    Review fixes

--- app/src/main/java/info/moevm/moodle/ui/entersetup/checksetup/UrlState.kt
+++ app/src/main/java/info/moevm/moodle/ui/entersetup/checksetup/UrlState.kt
@@ -3,7 +3,9 @@ package info.moevm.moodle.ui.entersetup.checksetup
 import info.moevm.moodle.ui.signin.TextFieldState
 import java.util.regex.Pattern
 
-private const val URL_VALIDATION_REGEX = """[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)"""
+// reference: https://stackoverflow.com/questions/3809401/what-is-a-good-regular-expression-to-match-a-url
+private const val URL_VALIDATION_REGEX =
+    """https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)"""
 
 class UrlState :
     TextFieldState(validator = ::isUrlValid, errorFor = ::urlValidationError)
